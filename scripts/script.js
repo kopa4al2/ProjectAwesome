6bf0dc686f7c1e4d236a0713fcec763409ed90ae
Stefan <a582hs@gmail.com>
2018-07-09 18:06:03 +0300

    Some updates
    Imported CraftyJS library
    Imported Plans about the game
    Developed canvas

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,38 @@
+let GAME_WIDTH;
+let GAME_HEIGHT;
 $(document).ready(() => {
-   $('li').on('click', function (item) {
 
-      // $(this).css('box-shadow', '1px 1px 8px #391439')
-      $(this).animate({
-         'box-shadow': '1px 1px 8px #391439'
-      },2000)
-   })
-})
+    let gameContainer = $('#game-container');
+
+    GAME_HEIGHT = gameContainer.height();
+    GAME_WIDTH = gameContainer.width();
+
+    //GAME LOOP
+
+    let assetObj = {
+        "sprites": {
+            "../images/util/arrow-fullscreen-box.png": {
+                tile: 960,
+                tileh: 960,
+                "exit_btn": [0, 0]
+            }
+        }
+    };
+
+    // Crafty.load("../images/util/arrow-fullscreen-box.png");
+
+    Crafty.init(GAME_WIDTH, GAME_HEIGHT, gameContainer[0]);
+    let exit = Crafty.e('2D, DOM, Image')
+        .image("../images/util/arrow-fullscreen-box.png")
+        .attr({x: GAME_WIDTH - 100, y: 0, w: 100, h: 100})
+        .bind("Click",
+            () => {
+                window.alert('Cyk');
+                console.log("cyk")
+            });
+    console.log(exit)
+    // Crafty.e('2D, Canvas, exit_btn').attr({x: GAME_WIDTH - 100, y: 0, w: 100, h: 100});
+
+
+});
 
